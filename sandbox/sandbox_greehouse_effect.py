8b16949626012f291d0c7d5a340abdc98b7919cd
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-02-22 14:51:52 -0300

    removed string nan from some columns

--- sandbox/sandbox_greehouse_effect.py
+++ sandbox/sandbox_greehouse_effect.py
@@ -3,4 +3,9 @@ df = pd.read_excel('/Users/rafaelsumiya/Downloads/greenhouse_effect.xlsx', sheet
 pd.set_option('display.max_columns', None)
 # print(df['Emissão / Remoção / Bunker'].unique())
 # print((df['Emissão / Remoção / Bunker'] == 'Remoção') | (df['Emissão / Remoção / Bunker'] == 'Remoção NCI'))
-print(df[df['Emissão / Remoção / Bunker'].isin(['Remoção', 'Remoção NCI'])])
+# print(df[df['Emissão / Remoção / Bunker'].isin(['Remoção', 'Remoção NCI'])])
+# print(df.loc[df['Emissão / Remoção / Bunker'].isin(['Remoção', 'Remoção NCI']), 1970:2021].max())
+# print(df.loc[df['Emissão / Remoção / Bunker'] == 'Bunker', 'Estado'].unique())
+df = df[df['Emissão / Remoção / Bunker'] == 'Emissão']
+df = df.drop(columns='Emissão / Remoção / Bunker')
+print(df)
--- transforms/magento/payout_entries.py
+++ transforms/magento/payout_entries.py
@@ -8,7 +8,8 @@ pd.set_option('display.max_columns', None)
 df = df.drop(['Vendor', 'Shipment ID', 'Entry Status', 'Transaction', 'Processed At'], axis=1)
 df.columns = df.columns.map(lambda x: x.lower().replace(' ', '_'))
 df = df.astype({'entry_id': str, 'vendor_id': str, 'marketplace': str, 'marketplace_id': str, 'shipment_increment': str, 'entry_type': str, 'entry_total': float, 'entry_message': str, 'created_at': str})
-df['created_at'] = df['created_at'].map(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S') if x != 'nan' else x.replace('nan', ''))
+df[['entry_id', 'vendor_id', 'marketplace', 'marketplace_id', 'shipment_increment', 'entry_type', 'entry_message', 'created_at']] = df[['entry_id', 'vendor_id', 'marketplace', 'marketplace_id', 'shipment_increment', 'entry_type', 'entry_message', 'created_at']].map(lambda x: x.replace('nan', ''))
+df['created_at'] = df['created_at'].map(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
 
 # Loading into Parquet
 df.to_parquet('/Users/rafaelsumiya/Downloads/payout_entries.parquet')
