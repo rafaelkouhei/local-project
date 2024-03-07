a87c01ef094b990cd0075ea8908c9e24a87e4dc8
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-03-07 14:59:47 -0300

    changed crm_afiliados fields from order to shipment

--- transforms/gsheets/crm_afiliados.py
+++ transforms/gsheets/crm_afiliados.py
@@ -2,12 +2,12 @@ import pandas as pd
 from datetime import datetime
 df = pd.read_csv('/Users/rafaelsumiya/Downloads/CRM Afiliados - Acompanhamento financeiro.csv')
 pd.set_option('display.max_columns', None)
-df = df[['Desconto', 'Order #', 'Order Date']]
-swap_name = {'Order #': 'order_id', 'Desconto': 'desconto', 'Order Date': 'order_date'}
+df = df[['Desconto', 'PO #', 'PO Date', 'PO Item SKU']]
+swap_name = {'PO #': 'shipment_id', 'Desconto': 'desconto', 'PO Date': 'shipment_date', 'PO Item SKU': 'sku'}
 df = df.rename(columns=swap_name)
-df['order_id'] = df['order_id'].astype('string')
+df['shipment_id'] = df['shipment_id'].astype('string')
 df['desconto'] = df['desconto'].map(lambda x: x.replace('%', ''))
 df['desconto'] = df['desconto'].astype('float')
 df['desconto'] = df['desconto'].map(lambda x: x / 100)
-df['order_date'] = df['order_date'].map(lambda x: datetime.strptime(x, '%d/%m/%Y'))
+df['shipment_date'] = df['shipment_date'].map(lambda x: datetime.strptime(x, '%d/%m/%Y'))
 df.to_parquet('/Users/rafaelsumiya/Downloads/crm_afiliados.parquet')
