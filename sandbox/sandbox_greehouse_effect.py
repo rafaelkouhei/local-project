5edfdfae67e13899d8101dd9d9b8677f7ec9c411
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2024-03-14 09:48:25 -0300

    added multi customer_msg

--- sandbox/sandbox_greehouse_effect.py
+++ sandbox/sandbox_greehouse_effect.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_excel('/Users/rafaelsumiya/Downloads/greenhouse_effect.xlsx', sheet_name='GEE Estados')
+df = pd.read_excel('/Users/rafaelsumiya/Downloads/greenhouse_effect.xlsx', sheet_name='GEE Estados', nrows=1000)
 pd.set_option('display.max_columns', None)
 # print(df['Emissão / Remoção / Bunker'].unique())
 # print((df['Emissão / Remoção / Bunker'] == 'Remoção') | (df['Emissão / Remoção / Bunker'] == 'Remoção NCI'))
@@ -15,7 +15,10 @@ df_gas = list(df.loc[:, 1970:2021].columns)
 df = df.melt(id_vars=df_info, value_vars=df_gas, var_name='Ano', value_name='Emissão')
 
 grouped_gas = df.groupby('Gás')[['Emissão']].sum().sort_values('Emissão', ascending=False)
-print(grouped_gas.groups)
-print(grouped_gas.get_group('CO2 (t)'))
-print(grouped_gas)
-print(grouped_gas.iloc[0:9].sum() / grouped_gas.sum())
+# print(grouped_gas.groups)
+# print(grouped_gas.get_group('CO2 (t)'))
+# print(grouped_gas)
+# print(grouped_gas.iloc[0:9].sum() / grouped_gas.sum())
+
+df_gas_by_section = df.groupby(['Gás', 'Nível 1 - Setor'])[['Emissão']].sum().sort_values('Emissão', ascending=False)
+print(df_gas_by_section)
---
+++ transforms/multi/customer_msg.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from unidecode import unidecode
+from datetime import datetime
+
+df = pd.read_excel('/Users/rafaelsumiya/Downloads/multi.xlsx')
+pd.set_option('display.max_columns', None)
+df = df.rename(columns=lambda x: unidecode(x.lower().replace(' ', '_').replace('(', '').replace(')', '')))
+df['numero_do_pedido'] = df['numero_do_pedido'].astype(str).map(lambda x: x[:9] if len(x) > 9 and x.startswith('0') else x)
+
+df['data_de_criacao'] = df['data_de_criacao'].map(lambda x: datetime.strptime(x[:10], '%d/%m/%Y'))
+print(df['data_de_criacao'])
+
+df.to_parquet('/Users/rafaelsumiya/Downloads/customer_msg.parquet')
